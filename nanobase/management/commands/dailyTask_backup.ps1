fb074ac003263663825b82829cc2db00b8d306c9
zhao27j <zhao27j@gmail.com>
2024-12-30 17:07:13 +0800

    Opt. in daily backup for db & uploads on Windows

--- nanobase/management/commands/dailyTask_backup.ps1
+++ nanobase/management/commands/dailyTask_backup.ps1
@@ -1,5 +1,13 @@
-$date = Get-Date -Format "yyyy-MM-dd"
-$destination = "C:\webDev\nanoDATA\daily_backup\Archive_$date.zip"
-$source_folder = "C:\webDev\nanoCMDB\uploads"
-$source_file = "C:\webDev\nanoCMDB\db.sqlite3"
-Compress-Archive -Path $source_folder,$source_file -DestinationPath $destination
+$src_file = "C:\webDev\nanoCMDB\db.sqlite3"
+$dest_folder = "C:\webDev\nanoDATA\daily_backup"
+Copy-Item -Path $src_file -Destination $dest_folder # copy the DB file to the destination folder
+
+$date = Get-Date -Format "yyyyMMdd" # "yyyy-MM-dd"
+$time = Get-Date -Format "HHmmss"
+
+$src_folder = "C:\webDev\nanoCMDB\uploads"
+$src_file = "C:\webDev\nanoDATA\daily_backup\db.sqlite3"
+$dest_file = "C:\webDev\nanoDATA\daily_backup\archive_${date}_${time}.zip"
+Compress-Archive -Path $src_folder, $src_file -DestinationPath $dest_file # compress the temporary folder
+
+Remove-Item -Path $src_file # remove the srouce file
