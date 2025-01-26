d3fa965c9e50b77280de212f7684b1ae321b8597
Jun ZHAO <zhao27j@gmail.com>
2025-01-26 13:18:33 +0800

    Opt. ssl/tls port for email_backend test

--- nanocmdb/settings.py
+++ nanocmdb/settings.py
@@ -165,11 +165,10 @@ environ.Env.read_env()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-
+# EMAIL_PORT = 465
+# EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
